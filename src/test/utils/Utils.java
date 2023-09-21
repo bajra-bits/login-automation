26443b481bb5c678b78fda19f0c1204cd257158e
Bajracharya <rejan.bajracharya@cotiviti.com>
2023-09-21 18:35:07 +0545

    fix: invalid password

--- src/test/utils/Utils.java
+++ src/test/utils/Utils.java
@@ -4,7 +4,7 @@ public class Utils {
     private static String email = "standard_user";
     private static String password = "secret_sauce";
     private static String invalidEmail = "secret_sauce";
-    private static String invalidPassword = "secret_sauce";
+    private static String invalidPassword = "secret_sauce_invalid";
 
     public static String getEmail() {
         return email;
